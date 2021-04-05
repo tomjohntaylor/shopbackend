9c08a3ab62e19003c7a10e2ac611ee351ee00fb6
Tomasz Krawczyk <tomasz.jan.krawczyk@gmail.com>
2021-04-05 22:38:39 +0200

    Initial commit

---
+++ saleprocess/tests.py
@@ -0,0 +1,20 @@
+
+
+# TODO: after user creation connected profile is created automatically
+# TODO: unauthorized user cant view current profile (403)
+# TODO: authorized user can view current profile (200) and it's his profile
+
+# TODO: unauthorized user can't create new order (403)
+# TODO: authorized user can create new order (200) and it's his orders
+# TODO: authorized user can create second order (200) and it's his orders
+# TODO: authorized user can't create order passing product id that doesnt exist
+# TODO: authorized user can view current profile orders (200) and it's there orders he created just before
+# TODO: unauthorized user can't see details of order (403)
+# TODO: authorized user can see details of order he created (200)
+# TODO: authorized user can delete an order he created (20X?) and on the order list there is only one order left
+# TODO: authorized user can edit an order he created (20X?) and on the order detail and order list order is edited
+# TODO: other user cant see others orders on order list view (403)
+# TODO: other user cant see others order detail (403)
+
+# TODO: unauthorized user can view product list and can only do get request
+# TODO: unauthorized user can view product detail and can only do get request
