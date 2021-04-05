7920967fbf8577681a002f8e39b3b8f73e31086a
Tomasz Krawczyk <tomasz.jan.krawczyk@gmail.com>
2021-04-06 01:50:26 +0200

    small fixes + provisioning initial commit

--- shopapi/settings.py
+++ shopapi/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import os
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "media"
