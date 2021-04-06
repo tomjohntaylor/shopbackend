d9f2bb03ea39a676357282d95ab98464bfef2bef
Tomasz Krawczyk <tomasz.jan.krawczyk@gmail.com>
2021-04-06 02:28:05 +0200

    small fixes + provisioning setup

--- shopapi/settings.py
+++ shopapi/settings.py
@@ -19,14 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-if 'DJANGO_DEBUG_FALSE' in os.environ:
+if os.environ['DJANGO_DEBUG_FALSE'] == 'y':
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:
     DEBUG = True
     SECRET_KEY = 'insecure-key-for-dev'
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['*']
 
 # Application definition
 
