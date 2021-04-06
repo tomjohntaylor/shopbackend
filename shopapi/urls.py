a0e9de8f8c2d9489b1cd73d3a6f6cdff963a3cbc
Tomasz Krawczyk <tomasz.jan.krawczyk@gmail.com>
2021-04-06 02:22:00 +0200

    small fixes + provisioning setup

--- shopapi/urls.py
+++ shopapi/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include("rest_framework.urls")),
-    path("api/", include("saleprocess.api.urls")),
-    path("api/", include("product.api.urls")),
+    path("", include("saleprocess.api.urls")),
+    path("", include("product.api.urls")),
 ]
